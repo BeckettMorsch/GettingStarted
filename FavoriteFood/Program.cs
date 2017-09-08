22081ee30ac2a7046fbfba9189bffd1c40ad68c5
BeckettMorsch <beckettmorsch@gmail.com>
2017-09-08 11:15:15 -0400

    Fixed Project

--- FavoriteFood/Program.cs
+++ FavoriteFood/Program.cs
@@ -12,11 +12,16 @@ namespace FavoriteFood
         {
             Console.WriteLine("What is your favorite food?");
             var food = Console.ReadLine();
-            if (food == "Pizza" || food == "pizza")
-                Console.WriteLine("Most excellent and correct!  Pizza is indeed one of the best foods ever put on this earth.");
+            
+            
+                if (food == "Pizza" || food == "pizza")
+                                   Console.WriteLine("Most excellent and correct!  Pizza is indeed one of the best foods ever put on this earth.");
+                    
+                
 
-            else
-                Console.WriteLine("Wrong!  That is a terrible choice and you should feel bad about yourself.   Your favorite should be pizza!");
+                else
+                    Console.WriteLine("Wrong!  That is a terrible choice and you should feel bad about yourself.   Your favorite should be pizza!");
+            
         }
     }
 }
--- PasswordAuthentication/Program.cs
+++ PasswordAuthentication/Program.cs
@@ -10,6 +10,32 @@ namespace PasswordAuthentication
     {
         static void Main(string[] args)
         {
+            var pass = "letmein";
+            var user = "root";
+            int num = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter the Username: ");
+                var userT = Console.ReadLine();
+                Console.WriteLine("Enter the Password: ");
+                var passT = Console.ReadLine();
+
+                if (userT == "root" && passT == "letmein")
+                {
+                    Console.WriteLine("Username and password authenticated.. you may proceed");
+                    num = 1;
+                    break;
+                }
+
+            }
+            if (num != 1)
+                Console.WriteLine("Too many incorrect login attempts!  Try again later!");
+            else
+                num = 0;
+
+        
+
         }
     }
 }
