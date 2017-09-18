19b163a8805d685594442d99f725ee6cf24e4ea0
BeckettMorsch <beckettmorsch@gmail.com>
2017-09-18 11:31:57 -0400

    Started Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,16 +11,41 @@ namespace Calculator
         static void Main(string[] args)
         {
             int first = 0;
+            int second = 0;
+            //int value;
+            int result = 0;
             Console.WriteLine("Enter an expression: ");
-            var input = Console.ReadLine();
+            String input = Console.ReadLine();
+           
             input = input.Replace(" ", String.Empty);
-            
+
             //if(length first = 1)
             first = Convert.ToInt32(input.Substring(0, 1));
             //else
-            first = Convert.ToInt32(input.Substring(0, 2));
+            second = Convert.ToInt32(input.Substring(2));
 
 
+
+
+
+            for (int i=0;i<input.Length + 1;i++)
+            {
+                if (input[i] == '*')
+                    result = first * second;
+
+                else if (input[i] == '/')
+                    result = first / second;
+
+                else if (input[i] == '+')
+                    result = first + second;
+                else
+                    result = first = second;
+
+                
+            }
+
+            Console.WriteLine("Result: " + input + " = "+result);
+            Console.WriteLine("hi");
 
 
 
